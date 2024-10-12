7c194d6f73533b4f5f217e563de921b20bb99028
ripjackie <theranslinkard@gmail.com>
2024-10-12 03:30:39 -0500

    workin on it

--- src/tacc.h
+++ src/tacc.h
@@ -1,17 +1,71 @@
 #ifndef  TACC_H_
 #define TACC_H_
 
+#include <any> // Needed??
 #include <string>
+#include <list>
+#include <memory>
 
 namespace tacc {
 
-class Tacc {
+typedef enum tokenType {
+    L_PAREN, R_PAREN, L_BRACE, R_BRACE,
+    COMMA, PERIOD, MINUS, PLUS, SEMICOLON, FSLASH, ASTERISK,
 
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    IDENTIFIER, STRING, NUMBER,
+
+    IF, ELSE, RETURN, DO, WHILE,
+
+    END
+} tokenType;
+
+struct Token {
+    tokenType type;
+    std::string lexeme;
+    std::any literal; // Better way??
+    int line;
+
+    static std::string typeToString(tokenType type);
+
+    public:
+    Token(tokenType type, std::string lexeme, std::any literal, int line);
+    std::string toString();
+};
+
+
+class Tacc {
     static void run(std::string buffer);
 
     public:
     static void runFile(std::string filename);
+    static void error(int line, std::string message);
+    static void report(int line, std::string where, std::string message);
+};
 
+class Lexer {
+    int start {0};
+    int current {0};
+    int line {1};
+    std::string source;
+    std::list<std::unique_ptr<Token>> tokens;
+
+    bool isAtEnd();
+    void scanToken();
+    char advance();
+    void addToken(tokenType type);
+    void addToken(tokenType type, std::any literal);
+    bool match(char expected);
+    char peek();
+
+    public:
+    Lexer(std::string source);
+
+    std::list<std::unique_ptr<Token>> lexTokens();
 };
 
 }
